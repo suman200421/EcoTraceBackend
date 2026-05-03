a68f85a61dcac678a2d45d2f25d3ecfd6c004916
suman200421 <padmanavakhamrui8479@gmail.com>
2026-05-03 20:10:00 +0530

    changed swagger for deployment

--- src/config/mail.js
+++ src/config/mail.js
@@ -5,10 +5,8 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true, // Use TLS instead of SSL
-  // requireTLS: true,
-  family: 4,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
